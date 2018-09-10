c0afbec846414222f141a76c89e0a2d67faf7962
locutus <31972936+locutus75@users.noreply.github.com>
2018-09-10 11:51:50 +0200

    added folder checks, formatting and cmd splitting

--- offlineupdatescript.sh
+++ offlineupdatescript.sh
@@ -1,35 +1,96 @@
 #!/bin/bash
-echo "Updating Syscoincore..."
-echo "Creating temp directory"
-mkdir ~/temp && cd ~/temp && git clone https://github.com/syscoin/syscoin -b master
+clear
+date_today=$(date +%F)
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+ORANGE='\033[1;33m'
+BLUE='\033[1;34m'
+PURPLE='\033[1;35m'
+CYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
+
+echo -e "${PURPLE}Updating Syscoincore${NC}"
+sleep 1
+echo -e "${CYAN}Creating temp directory${NC}"
+sleep 2
+if [ -d "/home/$USER/temp" ]
+then
+        echo -e "${ORANGE}Temp folder found, skipping${NC}"
+else
+        mkdir ~/temp
+        echo -e "${GREEN}Temp folder created.${NC}"
+fi
+cd ~/temp
 sleep 1
 
-echo "Start building."
-cd syscoin && ./autogen.sh && ./configure && make -j$(nproc) -pipe
-sleep 2
-
-echo "Setting up Sentinel"
-cd src && git clone https://github.com/syscoin/sentinel.git && cd sentinel
+echo -e "${CYAN}Downloading Syscoin-Core master${NC}"
+git clone https://github.com/syscoin/syscoin -b master
 sleep 1
 
-echo "Copying sentinel.conf"
-cp ~/syscoin/src/sentinel/sentinel.conf ~/temp/syscoin/src/sentinel/sentinel.conf 
+echo -e "${CYAN}Start building${NC}"
+cd syscoin
+echo -e "    ${CYAN}Generating...${NC}"
+sleep 2
+./autogen.sh
+
+echo -e "    ${CYAN}Configuring...${NC}"
+sleep 2
+./configure
+
+echo -e "    ${CYAN}Compiling...${NC}"
+sleep 2
+make -j$(nproc) -pipe
+
 sleep 2
 
-echo "Setting up sentinel"
+echo -e "${CYAN}Setting up Sentinel${NC}"
+cd src
+git clone https://github.com/syscoin/sentinel.git
+cd sentinel
+sleep 1
+
+echo -e "${CYAN}Copying sentinel.conf${NC}"
+cp ~/syscoin/src/sentinel/sentinel.conf ~/temp/syscoin/src/sentinel/sentinel.conf
+sleep 2
+
+echo -e "${CYAN}Setting up sentinel${NC}"
 virtualenv venv && venv/bin/pip install -r requirements.txt
 sleep 5
 
-echo "Shutting down Syscoincore"
-cd $home && ~/syscoin/src/syscoin-cli stop
+echo -e "${CYAN}Shutting down Syscoincore${NC}"
+cd $home
+~/syscoin/src/syscoin-cli stop
 sleep 10
 
-echo "Moving directories."
-mv ~/syscoin/ ~/temp/syscoin-previous && mv ~/temp/syscoin/ ~/syscoin && rm ~/.syscoincore/debug.log
-sleep 5
+echo -e "${CYAN}Checking previous folder${NC}"
+sleep 2
+if [ -d "/home/$USER/temp/syscoin-previous" ]
+then
+        echo -e "${GREEN}Previous folder found, renaming to syscoin-"$date_today${NC}
+        mv ~/temp/syscoin-previous ~/temp/syscoin-$date_today
+else
+        echo -e "${ORANGE}No previous update folder found, skipping.${NC}"
+fi
+sleep 2
 
-echo "Starting Syscoincore"
+echo -e "${CYAN}Moving directories${NC}"
+mv ~/syscoin ~/temp/syscoin-previous
+sleep 2
+mv ~/temp/syscoin ~/syscoin
+sleep 2
+
+echo -e "${CYAN}Removing old debug.log${NC}"
+rm ~/.syscoincore/debug.log
+sleep 2
+
+echo -e "${CYAN}Starting Syscoincore${NC}"
 ~/syscoin/src/syscoind
-echo "Done."
-echo "Tipping? alias: donations or address SRPz8SEEGQ7yXLGuRtMXDYPwagm8JuXrmG"
-echo "Thanks!"
+sleep 15
+
+echo -e "${RED}Running version is now:"
+./syscoin/src/syscoin-cli getinfo | grep \"version
+
+echo -e "${GREEN}Done.${NC}"
+echo -e "${CYAN}Tipping? alias: ${ORANGE}donations${CYAN} or use address ${ORANGE}SRPz8SEEGQ7yXLGuRtMXDYPwagm8JuXrmG${NC}"
+echo -e "${PURPLE}Thanks!${NC}"
