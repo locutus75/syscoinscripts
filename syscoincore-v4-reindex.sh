8fe913e14ebedc5c4e4257c7ecf0177b98ceecf9
locutus <31972936+locutus75@users.noreply.github.com>
2019-07-29 21:05:57 +0200

    New version for Syscoin v4

--- syscoincore-v4-reindex.sh
+++ syscoincore-v4-reindex.sh
@@ -9,7 +9,7 @@ PURPLE='\033[1;35m'
 CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 
-echo -e "${PURPLE}This script will restart Syscoincore and force reindex.${NC}"
+echo -e "${PURPLE}This script will restart Syscoincore v4 and force reindex.${NC}"
 echo -e "${ORANGE} may take around 30 minutes${NC}"
 echo -e "${ORANGE}  your mn status could be going to ${RED}Sentinel_Ping_Expired${ORANGE} but that is normal, it wil be ${GREEN}ENABLED${ORANGE} again within the hour${NC}"
 echo
@@ -30,40 +30,40 @@ if [ "$DO_CONTINUE" = "Y" ]
 			sleep 10
 
 			echo -e "${CYAN}Removing old debug.log${NC}"
-			if [ -e "/home/$USER/.syscoincore/debug.log" ]
+			if [ -e "/home/$USER/.syscoin/debug.log" ]
 			then
 							echo -e " ${GREEN}debug.log found, removing${NC}"
-							rm ~/.syscoincore/debug.log
+							rm ~/.syscoin/debug.log
 			else
 							echo -e " ${ORANGE}no debug.log found, skipping${NC}"
 			fi
 			sleep 2
 
 			echo -e "${CYAN}Removing previous peers.dat${NC}"
-			if [ -e "/home/$USER/.syscoincore/peers.dat" ]
+			if [ -e "/home/$USER/.syscoin/peers.dat" ]
 			then
 							echo -e " ${GREEN}peers.dat found, removing${NC}"
-							rm ~/.syscoincore/peers.dat
+							rm ~/.syscoin/peers.dat
 			else
 							echo -e " ${ORANGE}no peers.dat found, skipping${NC}"
 			fi
 			sleep 1
 
 			echo -e "${CYAN}Removing previous Blocks folder${NC}"
-			if [ -d "/home/$USER/.syscoincore/blocks" ]
+			if [ -d "/home/$USER/.syscoin/blocks" ]
 			then
 							echo -e " ${GREEN}Blocks folder found, removing${NC}"
-							rm -rf ~/.syscoincore/blocks
+							rm -rf ~/.syscoin/blocks
 			else
 							echo -e " ${ORANGE}no Blocks folder found, skipping${NC}"
 			fi
 			sleep 1
 
 			echo -e "${CYAN}Removing previous Chainstate folder${NC}"
-			if [ -d "/home/$USER/.syscoincore/chainstate" ]
+			if [ -d "/home/$USER/.syscoin/chainstate" ]
 			then
 							echo -e " ${GREEN}Chainstate found, removing${NC}"
-							rm -rf ~/.syscoincore/chainstate
+							rm -rf ~/.syscoin/chainstate
 			else
 							echo -e " ${ORANGE}no Chainstate folder found, skipping${NC}"
 			fi
@@ -74,7 +74,7 @@ if [ "$DO_CONTINUE" = "Y" ]
 			sleep 15
 
 			echo -e "${GREEN}Done.${NC}"
-			echo -e "${CYAN}Liked it? Syscoin Tippingjar alias: ${ORANGE}donations${CYAN} or use address ${ORANGE}SRPz8SEEGQ7yXLGuRtMXDYPwagm8JuXrmG${NC}"
+			echo -e "${CYAN}Liked it? Syscoin Tippingjar alias: ${ORANGE}donations${CYAN} or use address ${ORANGE}sys1qwtn246r9pfapmmccr7rncgperjc7qyadc9mz4s${NC}"
 			echo -e "${PURPLE}Thanks!${NC}"
 	else
 			echo -e "${CYAN}Scripted Installation detected, shutting down Syscoincore${NC}"
@@ -83,40 +83,40 @@ if [ "$DO_CONTINUE" = "Y" ]
 			sleep 10
 
 			echo -e "${CYAN}Removing old debug.log${NC}"
-			if [ -e "/home/syscoin/.syscoincore/debug.log" ]
+			if [ -e "/home/syscoin/.syscoin/debug.log" ]
 			then
 							echo -e " ${GREEN}debug.log found, removing${NC}"
-							sudo rm /home/syscoin/.syscoincore/debug.log
+							sudo rm /home/syscoin/.syscoin/debug.log
 			else
 							echo -e " ${ORANGE}no debug.log found, skipping${NC}"
 			fi
 			sleep 2
 
 			echo -e "${CYAN}Removing previous peers.dat${NC}"
-			if [ -e "/home/syscoin/.syscoincore/peers.dat" ]
+			if [ -e "/home/syscoin/.syscoin/peers.dat" ]
 			then
 							echo -e " ${GREEN}peers.dat found, removing${NC}"
-							sudo rm /home/syscoin/.syscoincore/peers.dat
+							sudo rm /home/syscoin/.syscoin/peers.dat
 			else
 							echo -e " ${ORANGE}no peers.dat found, skipping${NC}"
 			fi
 			sleep 1
 
 			echo -e "${CYAN}Removing previous Blocks folder${NC}"
-			if [ -d "/home/syscoin/.syscoincore/blocks" ]
+			if [ -d "/home/syscoin/.syscoin/blocks" ]
 			then
 							echo -e " ${GREEN}Blocks folder found, removing${NC}"
-							sudo rm -rf /home/syscoin/.syscoincore/blocks
+							sudo rm -rf /home/syscoin/.syscoin/blocks
 			else
 							echo -e " ${ORANGE}no Blocks folder found, skipping${NC}"
 			fi
 			sleep 1
 
 			echo -e "${CYAN}Removing previous Chainstate folder${NC}"
-			if [ -d "/home/syscoin/.syscoincore/chainstate" ]
+			if [ -d "/home/syscoin/.syscoin/chainstate" ]
 			then
 							echo -e " ${GREEN}Chainstate found, removing${NC}"
-							sudo rm -rf /home/syscoin/.syscoincore/chainstate
+							sudo rm -rf /home/syscoin/.syscoin/chainstate
 			else
 							echo -e " ${ORANGE}no Chainstate folder found, skipping${NC}"
 			fi
@@ -127,7 +127,7 @@ if [ "$DO_CONTINUE" = "Y" ]
 			sleep 15
 
 			echo -e "${GREEN}Done.${NC}"
-			echo -e "${CYAN}Liked it? Syscoin Tippingjar alias: ${ORANGE}donations${CYAN} or use address ${ORANGE}SRPz8SEEGQ7yXLGuRtMXDYPwagm8JuXrmG${NC}"
+			echo -e "${CYAN}Liked it? Syscoin Tippingjar alias: ${ORANGE}sys1qwtn246r9pfapmmccr7rncgperjc7qyadc9mz4s${NC}"
 			echo -e "${PURPLE}Thanks!${NC}"
 	
 	fi
