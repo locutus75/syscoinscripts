e6d2ef413fb9c0a807edfcf30533792b934f5002
locutus <31972936+locutus75@users.noreply.github.com>
2019-12-23 23:42:53 +0100

    banning based on string compare

--- scriptbanpre410nodes.sh
+++ scriptbanpre410nodes.sh
@@ -27,14 +27,15 @@ do
   # get the overall version string
   ver=$(echo $peer | awk -F '/' '{ print $2 }')
   # get the peer's main version
-  mainver=$(echo $ver | awk -F '.' '{ print $1 }' | cut -c 14)
+  #mainver=$(echo $ver | awk -F '.' '{ print $1 }' | cut -c 14)
   # get the peer's subversion
-  subver=$(echo $ver | awk -F '.' '{ print $2 }')
+  #subver=$(echo $ver | awk -F '.' '{ print $2 }')
   # get the peer's subsubversion
-  subsubver=$(echo $ver | awk -F '.' '{ print $3 }')
+  #subsubver=$(echo $ver | awk -F '.' '{ print $3 }')
 
   # if the version is lower than 4.1.0 then ban the node for one week (in seconds)
-  if [ $mainver -eq 4 ] && [ $subver -eq 1 ] && [ $subsubver -lt 0 ]
+  #if [ $mainver -eq 4 ] && [ $subver -eq 1 ] && [ $subsubver -lt 0 ]
+  if [ $ver != "Satoshi:4.1.0" ]
   then
   	if [ -d "/home/$USER/syscoin/src" ]
 		then
@@ -43,7 +44,7 @@ do
 		    	syscli setban $ip add 604800
   	fi
 	date
- 	echo $mainver"."$subver"."$subsubver"? BANNED! Bye bye" $ip"! See you in a week!"
+ 	echo "Found " $ver " - Bye bye" $ip"! See you in a week!"
 	echo
 fi
 
