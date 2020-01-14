79b07b278895580efb8378dada91399a89dae479
locutus <31972936+locutus75@users.noreply.github.com>
2020-01-14 13:02:54 +0100

    Create autoscript-reset-syscoincore.sh

---
+++ autoscript-reset-syscoincore.sh
@@ -0,0 +1,8 @@
+sudo service syscoind stop
+sleep 20
+cp /home/syscoin/.syscoin/syscoin.conf /home/syscoin/syscoin.org
+rm -rf /home/syscoin/.syscoin
+mkdir /home/syscoin/.syscoin
+cp /home/syscoin/syscoin.org /home/syscoin/.syscoin/syscoin.conf
+sleep 10
+sudo service syscoind start
