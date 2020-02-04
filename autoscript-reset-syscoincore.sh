3a2b343f9e4c44c2653fd5769605aeee46e7be39
locutus <31972936+locutus75@users.noreply.github.com>
2020-02-04 16:47:10 +0100

    added permissions for syscoin user

--- autoscript-reset-syscoincore.sh
+++ autoscript-reset-syscoincore.sh
@@ -3,6 +3,7 @@ sleep 20
 cp /home/syscoin/.syscoin/syscoin.conf /home/syscoin/syscoin.org
 rm -rf /home/syscoin/.syscoin
 mkdir /home/syscoin/.syscoin
+chown -R syscoin /home/syscoin/.syscoin
 cp /home/syscoin/syscoin.org /home/syscoin/.syscoin/syscoin.conf
 sleep 10
 sudo service syscoind start
