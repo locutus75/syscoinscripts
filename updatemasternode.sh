65e147f422b35e6cfe7c85e62fcc999c215433c4
locutus <31972936+locutus75@users.noreply.github.com>
2021-09-28 12:35:12 +0200

    TEMP - auto VER check override

--- updatemasternode.sh
+++ updatemasternode.sh
@@ -16,6 +16,7 @@ tag_grep=$(curl -sL $tag_url | grep -o -m1 "$tag_get\?[0-9]*\.[0-9]*\.[0-9]*")
 tag_ver=$(echo "$tag_grep" | cut -c$tag_pos-)
 
 VER=$tag_ver
+VER=4.2.2
 
 echo -e "${PURPLE}Updating Syscoin Masternode Versie ${VER}${NC}"
 sleep 5
