3315b521f0f453aa25562fd7d8c4646306c1affb
locutus <31972936+locutus75@users.noreply.github.com>
2021-11-24 22:48:01 +0100

    v3

--- versiontest.sh
+++ versiontest.sh
@@ -10,14 +10,14 @@ CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 
 tag_url="https://github.com/syscoin/syscoin/releases/latest/"
-echo tag_url
+echo $tag_url
 tag_get="tag_name=v"
-echo tag_get
+echo $tag_get
 tag_grep=$(curl -sL $tag_url | grep -o -m1 "$tag_get\?[0-9]*\.[0-9]*\.[0-9]*")
-echo tag_grep
+echo $tag_grep
 ((tag_pos=${#tag_get}+1))
 tag_ver=$(echo "$tag_grep" | cut -c$tag_pos-)
-echo tag_ver
+echo $tag_ver
 
 VER=$tag_ver
 
