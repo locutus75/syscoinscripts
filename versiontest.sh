3402f359831d5ea19e2ccdef06ccadefb72d945e
locutus <31972936+locutus75@users.noreply.github.com>
2021-11-24 22:37:49 +0100

    v1

---
+++ versiontest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+date_today=$(date +%F)
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+ORANGE='\033[1;33m'
+BLUE='\033[1;34m'
+PURPLE='\033[1;35m'
+CYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
+tag_url="https://github.com/syscoin/syscoin/releases/latest/"
+tag_get="tag_name=v"
+tag_grep=$(curl -sL $tag_url | grep -o -m1 "$tag_get\?[0-9]*\.[0-9]*\.[0-9]*")
+((tag_pos=${#tag_get}+1))
+tag_ver=$(echo "$tag_grep" | cut -c$tag_pos-)
+
+VER=$tag_ver
+
+echo -e "${PURPLE}Updating Syscoin Masternode Versie ${VER}${NC}"
