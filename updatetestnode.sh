0dfcd149fe8e47179148a8e51ce6e8658eb2916f
locutus <31972936+locutus75@users.noreply.github.com>
2022-08-23 15:36:58 +0200

    v4.4 rc4

--- updatetestnode.sh
+++ updatetestnode.sh
@@ -8,9 +8,9 @@ BLUE='\033[1;34m'
 PURPLE='\033[1;35m'
 CYAN='\033[1;36m'
 NC='\033[0m' # No Color
-RC=14
+RC=4
 
-echo -e "${PURPLE}Updating Syscoin Testnode RC${RC}${NC}"
+echo -e "${PURPLE}Updating Syscoin Testnode 4.4 RC${RC}${NC}"
 sleep 5
 
 echo -e "${CYAN}Shutting down Syscoincore...${NC}"
@@ -21,20 +21,20 @@ sleep 10
 cd $home
 
 echo -e "${CYAN}Downloading new Testnode RC${RC}${NC}"
-wget https://github.com/syscoin/syscoin/releases/download/v4.2.0rc${RC}/syscoin-4.2.0rc${RC}-x86_64-linux-gnu.tar.gz
+wget https://github.com/syscoin/syscoin/releases/download/v4.4.0rc${RC}/syscoin-4.4.0rc${RC}-x86_64-linux-gnu.tar.gz
 sleep 2
 
 echo -e "${CYAN}Unpacking...${NC}"
-tar xf syscoin-4.2.0rc${RC}-x86_64-linux-gnu.tar.gz
+tar xf syscoin-4.4.0rc${RC}-x86_64-linux-gnu.tar.gz
 
 echo -e "${CYAN}Installing...${NC}"
 sleep 2
-sudo install -m 0755 -o root -g root -t /usr/local/bin syscoin-4.2.0rc${RC}/bin/*
+sudo install -m 0755 -o root -g root -t /usr/local/bin syscoin-4.4.0rc${RC}/bin/*
 
 echo -e "${CYAN}Cleaning up...${NC}"
 sleep 2
-rm -r syscoin-4.2.0rc${RC}
-rm ~/syscoin-4.2.0rc${RC}-x86_64-linux-gnu.tar.gz
+rm -r syscoin-4.4.0rc${RC}
+rm ~/syscoin-4.4.0rc${RC}-x86_64-linux-gnu.tar.gz
 
 echo -e "${CYAN}Starting Syscoincore...${NC}"
 sleep 2
